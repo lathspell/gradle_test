5e5da7b4651567fb9f59e82684d4c8bc74f548a4
Christian Brunotte <cb@lathspell.de>
2019-04-07 20:15:45 +0200

    Further playing around with Kotlin DSL

--- java-gradle-kts/build.gradle.kts
+++ java-gradle-kts/build.gradle.kts
@@ -1,9 +1,9 @@
 plugins {
     java
     application             // https://docs.gradle.org/current/userguide/application_plugin.html
-    id("com.github.ben-manes.versions") version "0.21.0"  // https://github.com/ben-manes/gradle-versions-plugin for ":  dependencyUpdates"
-    id("com.github.johnrengelman.shadow") version "5.0.0" // https://imperceptiblethoughts.com/shadow/getting-started/ for ":shadowJar"
-    id("com.palantir.docker") version "0.21.0"            // https://github.com/palantir/gradle-docker/ for ":docker"
+    // id("com.github.ben-manes.versions") version "0.21.0"  // https://github.com/ben-manes/gradle-versions-plugin for ":  dependencyUpdates"
+    // id("com.github.johnrengelman.shadow") version "5.0.0" // https://imperceptiblethoughts.com/shadow/getting-started/ for ":shadowJar"
+    // id("com.palantir.docker") version "0.21.0"            // https://github.com/palantir/gradle-docker/ for ":docker"
 }
 
 group = "de.lathspell"
